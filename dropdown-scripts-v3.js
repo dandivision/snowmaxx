fd02139fc8d7580c60373afa5aee10dff86455f1
dandivision <145878502+dandivision@users.noreply.github.com>
2023-09-24 02:43:49 +0200

    Update and rename dropdown-scripts-v2.js to dropdown-scripts-v3.js

--- dropdown-scripts-v3.js
+++ dropdown-scripts-v3.js
@@ -1,217 +1,214 @@
 // DROPDOWN menu Summer Camp
-var enterTimeline = gsap.timeline({ paused: true });
-var navLink = document.getElementById('hero-navlink-summer-camp');
-var isAnimationReversed = true; // L'animazione inizia come invertita
+var enterTimelineSummerCamp = gsap.timeline({ paused: true });
+var navLinkSummerCamp = document.getElementById('hero-navlink-summer-camp');
+var isAnimationReversedSummerCamp = true; // L'animazione inizia come invertita
 
 // Aggiungiamo le azioni all'animazione di entrata con easing "ease"
-enterTimeline
+enterTimelineSummerCamp
   .fromTo('.hero-navbar_dropdown', { display: 'none', opacity: 0 }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' })
   .fromTo('.dropdown_menu-component.is-summer-camp', { display: 'none', opacity: 0 }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' }, '-=0.3')
   .fromTo('.dropdown_menu-image.is-summer-camp', { scale: 1.3 }, { scale: 1, duration: 0.3, ease: 'ease' }, '-=0.2')
   .fromTo('.dropdown_menu-text-wrapper.is-summer-camp', { y: '7%', opacity: 0 }, { y: '0%', opacity: 1, duration: 0.3, ease: 'ease' }, '-=0.2');
 
 // Funzione per avviare l'animazione di entrata e gestire lo stato di hover
-function playEnterAnimation() {
+function playEnterAnimationSummerCamp() {
   // Avvia l'animazione di entrata
-  enterTimeline.play();
-  isAnimationReversed = false;
+  enterTimelineSummerCamp.play();
+  isAnimationReversedSummerCamp = false;
 
   // Imposta l'id in stato di hover
-  navLink.classList.add('hovered');
+  navLinkSummerCamp.classList.add('hovered');
 }
 
 // Funzione per eseguire il reverse dell'animazione di uscita e gestire lo stato di hover
-function reverseExitAnimation() {
-  enterTimeline.reverse(0.28);
-  isAnimationReversed = true;
+function reverseExitAnimationSummerCamp() {
+  enterTimelineSummerCamp.reverse(0.28);
+  isAnimationReversedSummerCamp = true;
 
   // Rimuovi lo stato di hover quando l'animazione è invertita
-  navLink.classList.remove('hovered');
+  navLinkSummerCamp.classList.remove('hovered');
 }
 
 // Aggiungiamo l'evento click per attivare l'animazione di entrata
-navLink.addEventListener('click', function() {
+navLinkSummerCamp.addEventListener('click', function() {
   // Avvia l'animazione di entrata
-  playEnterAnimation();
+  playEnterAnimationSummerCamp();
 });
 
 // Aggiungiamo l'evento mouseleave su .dropdown_menu-component.is-summer-camp per riavviare l'animazione di uscita
-document.querySelector('.dropdown_menu-component.is-summer-camp').addEventListener('mouseleave', reverseExitAnimation);
+document.querySelector('.dropdown_menu-component.is-summer-camp').addEventListener('mouseleave', reverseExitAnimationSummerCamp);
 
 // Aggiungiamo l'evento mouseenter su .hero-navbar_link tranne quelle con id hero-navlink-summer-camp
-var heroNavLinks = document.querySelectorAll('.hero-navbar_link');
-for (var i = 0; i < heroNavLinks.length; i++) {
-  var link = heroNavLinks[i];
+var heroNavLinksSummerCamp = document.querySelectorAll('.hero-navbar_link');
+for (var i = 0; i < heroNavLinksSummerCamp.length; i++) {
+  var link = heroNavLinksSummerCamp[i];
   if (link.id !== 'hero-navlink-summer-camp') {
     link.addEventListener('mouseenter', function() {
       // Esegui il reverse dell'animazione solo se l'animazione non è stata invertita
-      if (!isAnimationReversed) {
-        reverseExitAnimation();
+      if (!isAnimationReversedSummerCamp) {
+        reverseExitAnimationSummerCamp();
       }
     });
   }
 }
 
-
 // DROPDOWN menu Lesson
-var enterTimeline2 = gsap.timeline({ paused: true });
-var navLink2 = document.getElementById('hero-navlink-lesson');
-var isAnimationReversed2 = true; // L'animazione inizia come invertita
+var enterTimelineLesson = gsap.timeline({ paused: true });
+var navLinkLesson = document.getElementById('hero-navlink-lesson');
+var isAnimationReversedLesson = true; // L'animazione inizia come invertita
 
 // Aggiungiamo le azioni all'animazione di entrata con easing "ease"
-enterTimeline2
+enterTimelineLesson
   .fromTo('.hero-navbar_dropdown', { display: 'none', opacity: 0 }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' })
   .fromTo('.dropdown_menu-component.is-lesson', { display: 'none', opacity: 0 }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' }, '-=0.3')
   .fromTo('.dropdown_menu-image.is-lesson', { scale: 1.3 }, { scale: 1, duration: 0.3, ease: 'ease' }, '-=0.2')
   .fromTo('.dropdown_menu-text-wrapper.is-lesson', { y: '7%', opacity: 0 }, { y: '0%', opacity: 1, duration: 0.3, ease: 'ease' }, '-=0.2');
 
 // Funzione per avviare l'animazione di entrata e gestire lo stato di hover
-function playEnterAnimation() {
+function playEnterAnimationLesson() {
   // Avvia l'animazione di entrata
-  enterTimeline2.play();
-  isAnimationReversed2 = false;
+  enterTimelineLesson.play();
+  isAnimationReversedLesson = false;
 
   // Imposta l'id in stato di hover
-  navLink2.classList.add('hovered');
+  navLinkLesson.classList.add('hovered');
 }
 
 // Funzione per eseguire il reverse dell'animazione di uscita e gestire lo stato di hover
-function reverseExitAnimation() {
-  enterTimeline2.reverse(0.28);
-  isAnimationReversed2 = true;
+function reverseExitAnimationLesson() {
+  enterTimelineLesson.reverse(0.28);
+  isAnimationReversedLesson = true;
 
   // Rimuovi lo stato di hover quando l'animazione è invertita
-  navLink2.classList.remove('hovered');
+  navLinkLesson.classList.remove('hovered');
 }
 
 // Aggiungiamo l'evento click per attivare l'animazione di entrata
-navLink2.addEventListener('click', function() {
+navLinkLesson.addEventListener('click', function() {
   // Avvia l'animazione di entrata
-  playEnterAnimation();
+  playEnterAnimationLesson();
 });
 
 // Aggiungiamo l'evento mouseleave su .dropdown_menu-component.is-lesson per riavviare l'animazione di uscita
-document.querySelector('.dropdown_menu-component.is-lesson').addEventListener('mouseleave', reverseExitAnimation);
+document.querySelector('.dropdown_menu-component.is-lesson').addEventListener('mouseleave', reverseExitAnimationLesson);
 
 // Aggiungiamo l'evento mouseenter su .hero-navbar_link tranne quelle con id hero-navlink-lesson
-var heroNavLinks2 = document.querySelectorAll('.hero-navbar_link');
-for (var i = 0; i < heroNavLinks2.length; i++) {
-  var link = heroNavLinks2[i];
+var heroNavLinksLesson = document.querySelectorAll('.hero-navbar_link');
+for (var i = 0; i < heroNavLinksLesson.length; i++) {
+  var link = heroNavLinksLesson[i];
   if (link.id !== 'hero-navlink-lesson') {
     link.addEventListener('mouseenter', function() {
       // Esegui il reverse dell'animazione solo se l'animazione non è stata invertita
-      if (!isAnimationReversed2) {
-        reverseExitAnimation();
+      if (!isAnimationReversedLesson) {
+        reverseExitAnimationLesson();
       }
     });
   }
 }
 
-
 // DROPDOWN menu Courses
-var enterTimeline3 = gsap.timeline({ paused: true });
-var navLink3 = document.getElementById('hero-navlink-courses');
-var isAnimationReversed3 = true; // L'animazione inizia come invertita
+var enterTimelineCourses = gsap.timeline({ paused: true });
+var navLinkCourses = document.getElementById('hero-navlink-courses');
+var isAnimationReversedCourses = true; // L'animazione inizia come invertita
 
 // Aggiungiamo le azioni all'animazione di entrata con easing "ease"
-enterTimeline3
+enterTimelineCourses
   .fromTo('.hero-navbar_dropdown', { display: 'none', opacity: 0 }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' })
   .fromTo('.dropdown_menu-component.is-courses', { display: 'none', opacity: 0 }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' }, '-=0.3')
   .fromTo('.dropdown_menu-image.is-courses', { scale: 1.3 }, { scale: 1, duration: 0.3, ease: 'ease' }, '-=0.2')
   .fromTo('.dropdown_menu-text-wrapper.is-courses', { y: '7%', opacity: 0 }, { y: '0%', opacity: 1, duration: 0.3, ease: 'ease' }, '-=0.2');
 
 // Funzione per avviare l'animazione di entrata e gestire lo stato di hover
-function playEnterAnimation() {
+function playEnterAnimationCourses() {
   // Avvia l'animazione di entrata
-  enterTimeline3.play();
-  isAnimationReversed3 = false;
+  enterTimelineCourses.play();
+  isAnimationReversedCourses = false;
 
   // Imposta l'id in stato di hover
-  navLink3.classList.add('hovered');
+  navLinkCourses.classList.add('hovered');
 }
 
 // Funzione per eseguire il reverse dell'animazione di uscita e gestire lo stato di hover
-function reverseExitAnimation() {
-  enterTimeline3.reverse(0.28);
-  isAnimationReversed3 = true;
+function reverseExitAnimationCourses() {
+  enterTimelineCourses.reverse(0.28);
+  isAnimationReversedCourses = true;
 
   // Rimuovi lo stato di hover quando l'animazione è invertita
-  navLink3.classList.remove('hovered');
+  navLinkCourses.classList.remove('hovered');
 }
 
 // Aggiungiamo l'evento click per attivare l'animazione di entrata
-navLink3.addEventListener('click', function() {
+navLinkCourses.addEventListener('click', function() {
   // Avvia l'animazione di entrata
-  playEnterAnimation();
+  playEnterAnimationCourses();
 });
 
 // Aggiungiamo l'evento mouseleave su .dropdown_menu-component.is-courses per riavviare l'animazione di uscita
-document.querySelector('.dropdown_menu-component.is-courses').addEventListener('mouseleave', reverseExitAnimation);
+document.querySelector('.dropdown_menu-component.is-courses').addEventListener('mouseleave', reverseExitAnimationCourses);
 
 // Aggiungiamo l'evento mouseenter su .hero-navbar_link tranne quelle con id hero-navlink-courses
-var heroNavLinks3 = document.querySelectorAll('.hero-navbar_link');
-for (var i = 0; i < heroNavLinks.length; i++) {
-  var link = heroNavLinks3[i];
+var heroNavLinksCourses = document.querySelectorAll('.hero-navbar_link');
+for (var i = 0; i < heroNavLinksCourses.length; i++) {
+  var link = heroNavLinksCourses[i];
   if (link.id !== 'hero-navlink-courses') {
     link.addEventListener('mouseenter', function() {
       // Esegui il reverse dell'animazione solo se l'animazione non è stata invertita
-      if (!isAnimationReversed3) {
-        reverseExitAnimation();
+      if (!isAnimationReversedCourses) {
+        reverseExitAnimationCourses();
       }
     });
   }
 }
 
-
 // DROPDOWN menu Academy
-var enterTimeline4 = gsap.timeline({ paused: true });
-var navLink4 = document.getElementById('hero-navlink-academy');
-var isAnimationReversed4 = true; // L'animazione inizia come invertita
+var enterTimelineAcademy = gsap.timeline({ paused: true });
+var navLinkAcademy = document.getElementById('hero-navlink-academy');
+var isAnimationReversedAcademy = true; // L'animazione inizia come invertita
 
 // Aggiungiamo le azioni all'animazione di entrata con easing "ease"
-enterTimeline4
+enterTimelineAcademy
   .fromTo('.hero-navbar_dropdown', { display: 'none', opacity: 0 }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' })
   .fromTo('.dropdown_menu-component.is-academy', { display: 'none', opacity: 0 }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' }, '-=0.3')
   .fromTo('.dropdown_menu-image.is-academy', { scale: 1.3 }, { scale: 1, duration: 0.3, ease: 'ease' }, '-=0.2')
   .fromTo('.dropdown_menu-text-wrapper.is-academy', { y: '7%', opacity: 0 }, { y: '0%', opacity: 1, duration: 0.3, ease: 'ease' }, '-=0.2');
 
 // Funzione per avviare l'animazione di entrata e gestire lo stato di hover
-function playEnterAnimation() {
+function playEnterAnimationAcademy() {
   // Avvia l'animazione di entrata
-  enterTimeline4.play();
-  isAnimationReversed4 = false;
+  enterTimelineAcademy.play();
+  isAnimationReversedAcademy = false;
 
   // Imposta l'id in stato di hover
-  navLink4.classList.add('hovered');
+  navLinkAcademy.classList.add('hovered');
 }
 
 // Funzione per eseguire il reverse dell'animazione di uscita e gestire lo stato di hover
-function reverseExitAnimation() {
-  enterTimeline4.reverse(0.28);
-  isAnimationReversed4 = true;
+function reverseExitAnimationAcademy() {
+  enterTimelineAcademy.reverse(0.28);
+  isAnimationReversedAcademy = true;
 
   // Rimuovi lo stato di hover quando l'animazione è invertita
-  navLink4.classList.remove('hovered');
+  navLinkAcademy.classList.remove('hovered');
 }
 
 // Aggiungiamo l'evento click per attivare l'animazione di entrata
-navLink4.addEventListener('click', function() {
+navLinkAcademy.addEventListener('click', function() {
   // Avvia l'animazione di entrata
-  playEnterAnimation();
+  playEnterAnimationAcademy();
 });
 
 // Aggiungiamo l'evento mouseleave su .dropdown_menu-component.is-academy per riavviare l'animazione di uscita
-document.querySelector('.dropdown_menu-component.is-academy').addEventListener('mouseleave', reverseExitAnimation);
+document.querySelector('.dropdown_menu-component.is-academy').addEventListener('mouseleave', reverseExitAnimationAcademy);
 
 // Aggiungiamo l'evento mouseenter su .hero-navbar_link tranne quelle con id hero-navlink-academy
-var heroNavLinks4 = document.querySelectorAll('.hero-navbar_link');
-for (var i = 0; i < heroNavLinks4.length; i++) {
-  var link = heroNavLinks[i];
+var heroNavLinksAcademy = document.querySelectorAll('.hero-navbar_link');
+for (var i = 0; i < heroNavLinksAcademy.length; i++) {
+  var link = heroNavLinksAcademy[i];
   if (link.id !== 'hero-navlink-academy') {
     link.addEventListener('mouseenter', function() {
       // Esegui il reverse dell'animazione solo se l'animazione non è stata invertita
-      if (!isAnimationReversed4) {
-        reverseExitAnimation();
+      if (!isAnimationReversedAcademy) {
+        reverseExitAnimationAcademy();
       }
     });
   }
