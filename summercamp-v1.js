91cb12121c9f08de79b51f6019fe24a69f3c7610
dandivision <145878502+dandivision@users.noreply.github.com>
2023-10-04 00:46:38 +0200

    Update and rename summercamp-dd-8.js to summercamp-v1.js

--- summercamp-v1.js
+++ summercamp-v1.js
@@ -1,53 +1,53 @@
 // DROPDOWN menu Summer Camp
 var enterTimelineSummerCamp = gsap.timeline({ paused: true });
 var navLinkSummerCamp = document.getElementById('hero-navlink-summer-camp');
-var isAnimationReversedSummerCamp = true; // Animation starts in reversed state
+var isAnimationReversedSummerCamp = true; // L'animazione inizia come invertita
 
-// Add actions to the entrance animation with "ease" easing
+// Aggiungiamo le azioni all'animazione di entrata con easing "ease"
 enterTimelineSummerCamp
   .fromTo('.hero-navbar_dropdown', { display: 'none', opacity: 0 }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' })
   .fromTo('.dropdown_menu-component.is-summer-camp', { display: 'none', opacity: 0 }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' }, '-=0.3')
   .fromTo('.dropdown_menu-image.is-summer-camp', { scale: 1.3 }, { scale: 1, duration: 0.3, ease: 'ease' }, '-=0.2')
   .fromTo('.dropdown_menu-text-wrapper.is-summer-camp', { y: '7%', opacity: 0 }, { y: '0%', opacity: 1, duration: 0.3, ease: 'ease' }, '-=0.2');
 
-// Function to start the entrance animation and handle hover state
+// Funzione per avviare l'animazione di entrata e gestire lo stato di hover
 function playEnterAnimationSummerCamp() {
-  // Start the entrance animation
+  // Avvia l'animazione di entrata
   enterTimelineSummerCamp.play();
   isAnimationReversedSummerCamp = false;
 
-  // Set the id in hover state
+  // Imposta l'id in stato di hover
   navLinkSummerCamp.classList.add('hovered');
 }
 
-// Function to execute the reverse of the exit animation and handle hover state
+// Funzione per eseguire il reverse dell'animazione di uscita e gestire lo stato di hover
 function reverseExitAnimationSummerCamp() {
-  // Set the animation state to reversed
+  enterTimelineSummerCamp.reverse(0.28);
   isAnimationReversedSummerCamp = true;
 
-  // Hide the elements on hover out
-  gsap.set('.hero-navbar_dropdown, .dropdown_menu-component.is-summer-camp', { display: 'none', opacity: 0 });
-
-  // Remove hover state when the animation is "reversed"
+  // Rimuovi lo stato di hover quando l'animazione è invertita
   navLinkSummerCamp.classList.remove('hovered');
+
+  // Nascondi anche la classe .hero-navbar_dropdown
+  gsap.to('.hero-navbar_dropdown', { display: 'none', duration: 0 });
 }
 
-// Add the hover event to activate the entrance animation
+// Aggiungiamo l'evento mouseenter per attivare l'animazione di entrata
 navLinkSummerCamp.addEventListener('mouseenter', function() {
-  // Start the entrance animation
+  // Avvia l'animazione di entrata
   playEnterAnimationSummerCamp();
 });
 
-// Add the hover event to hide elements on hover out
+// Aggiungiamo l'evento mouseleave su .dropdown_menu-component.is-summer-camp per riavviare l'animazione di uscita
 document.querySelector('.dropdown_menu-component.is-summer-camp').addEventListener('mouseleave', reverseExitAnimationSummerCamp);
 
-// Add the hover event to .hero-navbar_link except those with id hero-navlink-summer-camp
+// Aggiungiamo l'evento mouseenter su .hero-navbar_link tranne quelle con id hero-navlink-summer-camp
 var heroNavLinksSummerCamp = document.querySelectorAll('.hero-navbar_link');
 for (var i = 0; i < heroNavLinksSummerCamp.length; i++) {
   var link = heroNavLinksSummerCamp[i];
   if (link.id !== 'hero-navlink-summer-camp') {
     link.addEventListener('mouseenter', function() {
-      // Hide elements only if the animation has not been "reversed"
+      // Esegui il reverse dell'animazione solo se l'animazione non è stata invertita
       if (!isAnimationReversedSummerCamp) {
         reverseExitAnimationSummerCamp();
       }
@@ -55,13 +55,7 @@ for (var i = 0; i < heroNavLinksSummerCamp.length; i++) {
   }
 }
 
-// Add onComplete callback to handle the end of the animation
+// Aggiungi un callback per riprodurre l'animazione ogni volta che viene completata
 enterTimelineSummerCamp.eventCallback("onComplete", function() {
-  // Additional logic to execute after the animation ends
-
-  // You can add more logic here if needed
-
-  // Reset the timeline to the beginning for the next trigger
-  enterTimelineSummerCamp.seek(0);
-  enterTimelineSummerCamp.pause();
+  isAnimationReversedSummerCamp = true;
 });
