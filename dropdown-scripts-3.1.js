81080ff1c2ae6dd48a6beb57bb8725e6e1b7392f
dandivision <145878502+dandivision@users.noreply.github.com>
2023-10-04 02:30:42 +0200

    Update and rename dropdown-scripts-3.0.js to dropdown-scripts-3.1.js

--- dropdown-scripts-3.1.js
+++ dropdown-scripts-3.1.js
@@ -1,10 +1,30 @@
+// Common function to close active dropdown
+function closeActiveDropdown(dropdown, timeline, navLink) {
+  gsap.set('.hero-navbar_dropdown', { display: 'none' });
+  gsap.set(dropdown, { ...initialValues.dropdown });
+  gsap.set('.dropdown_menu-image', { ...initialValues.image });
+  gsap.set('.dropdown_menu-text-wrapper', { ...initialValues.textWrapper });
+  timeline.reverse();
+  isAnimationReversed = true;
+  navLink.classList.remove('hovered');
+}
+
+// Common function to set active dropdown
+function setActiveDropdown(dropdown, timeline, navLink) {
+  closeActiveDropdown();
+  dropdown.style.display = 'block';
+  timeline.restart();
+  isAnimationReversed = false;
+  navLink.classList.add('hovered');
+}
+
 // DROPDOWN menu Summer Camp
 var enterTimelineSummerCamp = gsap.timeline({ paused: true });
 var navLinkSummerCamp = document.getElementById('hero-navlink-summer-camp');
 var isAnimationReversedSummerCamp = true; // Animation starts as reversed
 
 // Capture initial values for the reverseExitAnimationSummerCamp function
-var initialValues = {
+var initialValuesSummerCamp = {
   dropdown: { display: 'none', opacity: 0 },
   image: { scale: 1.3 },
   textWrapper: { opacity: 0, y: '7%' }
@@ -13,34 +33,19 @@ var initialValues = {
 // Add actions to the entrance animation with easing "ease"
 enterTimelineSummerCamp
   .fromTo('.hero-navbar_dropdown', { display: 'none', opacity: 0 }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' })
-  .fromTo('.dropdown_menu-component.is-summer-camp', { ...initialValues.dropdown }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' }, '-=0.3')
-  .fromTo('.dropdown_menu-image.is-summer-camp', { ...initialValues.image }, { scale: 1, duration: 0.3, ease: 'ease' }, '-=0.2')
-  .fromTo('.dropdown_menu-text-wrapper.is-summer-camp', { ...initialValues.textWrapper }, { opacity: 1, y: '0%', duration: 0.3, ease: 'ease' }, '-=0.2');
+  .fromTo('.dropdown_menu-component.is-summer-camp', { ...initialValuesSummerCamp.dropdown }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' }, '-=0.3')
+  .fromTo('.dropdown_menu-image.is-summer-camp', { ...initialValuesSummerCamp.image }, { scale: 1, duration: 0.3, ease: 'ease' }, '-=0.2')
+  .fromTo('.dropdown_menu-text-wrapper.is-summer-camp', { ...initialValuesSummerCamp.textWrapper }, { opacity: 1, y: '0%', duration: 0.3, ease: 'ease' }, '-=0.2');
 
 // Function to start the entrance animation and handle hover state
 function playEnterAnimationSummerCamp() {
   // Restart the animation from the beginning
-  enterTimelineSummerCamp.restart();
-  isAnimationReversedSummerCamp = false;
-
-  // Set the id to the hovered state
-  navLinkSummerCamp.classList.add('hovered');
+  setActiveDropdown('.dropdown_menu-component.is-summer-camp', enterTimelineSummerCamp, navLinkSummerCamp);
 }
 
 // Function to reverse the exit animation and handle hover state
 function reverseExitAnimationSummerCamp() {
-  // Hide only the target class .hero-navbar_dropdown instead of reversing the timeline
-  gsap.set('.hero-navbar_dropdown', { display: 'none' });
-
-  // Restore the initial values of the target classes
-  gsap.set('.dropdown_menu-component.is-summer-camp', { ...initialValues.dropdown });
-  gsap.set('.dropdown_menu-image.is-summer-camp', { ...initialValues.image });
-  gsap.set('.dropdown_menu-text-wrapper.is-summer-camp', { ...initialValues.textWrapper });
-
-  isAnimationReversedSummerCamp = true;
-
-  // Remove the hover state when the animation is reversed
-  navLinkSummerCamp.classList.remove('hovered');
+  closeActiveDropdown('.dropdown_menu-component.is-summer-camp', enterTimelineSummerCamp, navLinkSummerCamp);
 }
 
 // Add mouseenter event to trigger the entrance animation
@@ -71,14 +76,13 @@ enterTimelineSummerCamp.eventCallback("onComplete", function() {
   isAnimationReversedSummerCamp = true;
 });
 
-
 // DROPDOWN menu Lesson
 var enterTimelineLesson = gsap.timeline({ paused: true });
 var navLinkLesson = document.getElementById('hero-navlink-lesson');
 var isAnimationReversedLesson = true; // Animation starts as reversed
 
 // Capture initial values for the reverseExitAnimationLesson function
-var initialValues = {
+var initialValuesLesson = {
   dropdown: { display: 'none', opacity: 0 },
   image: { scale: 1.3 },
   textWrapper: { opacity: 0, y: '7%' }
@@ -87,34 +91,19 @@ var initialValues = {
 // Add actions to the entrance animation with easing "ease"
 enterTimelineLesson
   .fromTo('.hero-navbar_dropdown', { display: 'none', opacity: 0 }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' })
-  .fromTo('.dropdown_menu-component.is-lesson', { ...initialValues.dropdown }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' }, '-=0.3')
-  .fromTo('.dropdown_menu-image.is-lesson', { ...initialValues.image }, { scale: 1, duration: 0.3, ease: 'ease' }, '-=0.2')
-  .fromTo('.dropdown_menu-text-wrapper.is-lesson', { ...initialValues.textWrapper }, { opacity: 1, y: '0%', duration: 0.3, ease: 'ease' }, '-=0.2');
+  .fromTo('.dropdown_menu-component.is-lesson', { ...initialValuesLesson.dropdown }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' }, '-=0.3')
+  .fromTo('.dropdown_menu-image.is-lesson', { ...initialValuesLesson.image }, { scale: 1, duration: 0.3, ease: 'ease' }, '-=0.2')
+  .fromTo('.dropdown_menu-text-wrapper.is-lesson', { ...initialValuesLesson.textWrapper }, { opacity: 1, y: '0%', duration: 0.3, ease: 'ease' }, '-=0.2');
 
 // Function to start the entrance animation and handle hover state
 function playEnterAnimationLesson() {
   // Restart the animation from the beginning
-  enterTimelineLesson.restart();
-  isAnimationReversedLesson = false;
-
-  // Set the id to the hovered state
-  navLinkLesson.classList.add('hovered');
+  setActiveDropdown('.dropdown_menu-component.is-lesson', enterTimelineLesson, navLinkLesson);
 }
 
 // Function to reverse the exit animation and handle hover state
 function reverseExitAnimationLesson() {
-  // Hide only the target class .hero-navbar_dropdown instead of reversing the timeline
-  gsap.set('.hero-navbar_dropdown', { display: 'none' });
-
-  // Restore the initial values of the target classes
-  gsap.set('.dropdown_menu-component.is-lesson', { ...initialValues.dropdown });
-  gsap.set('.dropdown_menu-image.is-lesson', { ...initialValues.image });
-  gsap.set('.dropdown_menu-text-wrapper.is-lesson', { ...initialValues.textWrapper });
-
-  isAnimationReversedLesson = true;
-
-  // Remove the hover state when the animation is reversed
-  navLinkLesson.classList.remove('hovered');
+  closeActiveDropdown('.dropdown_menu-component.is-lesson', enterTimelineLesson, navLinkLesson);
 }
 
 // Add mouseenter event to trigger the entrance animation
@@ -145,15 +134,13 @@ enterTimelineLesson.eventCallback("onComplete", function() {
   isAnimationReversedLesson = true;
 });
 
-
-
 // DROPDOWN menu Courses
 var enterTimelineCourses = gsap.timeline({ paused: true });
 var navLinkCourses = document.getElementById('hero-navlink-courses');
 var isAnimationReversedCourses = true; // Animation starts as reversed
 
 // Capture initial values for the reverseExitAnimationCourses function
-var initialValues = {
+var initialValuesCourses = {
   dropdown: { display: 'none', opacity: 0 },
   image: { scale: 1.3 },
   textWrapper: { opacity: 0, y: '7%' }
@@ -162,34 +149,19 @@ var initialValues = {
 // Add actions to the entrance animation with easing "ease"
 enterTimelineCourses
   .fromTo('.hero-navbar_dropdown', { display: 'none', opacity: 0 }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' })
-  .fromTo('.dropdown_menu-component.is-courses', { ...initialValues.dropdown }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' }, '-=0.3')
-  .fromTo('.dropdown_menu-image.is-courses', { ...initialValues.image }, { scale: 1, duration: 0.3, ease: 'ease' }, '-=0.2')
-  .fromTo('.dropdown_menu-text-wrapper.is-courses', { ...initialValues.textWrapper }, { opacity: 1, y: '0%', duration: 0.3, ease: 'ease' }, '-=0.2');
+  .fromTo('.dropdown_menu-component.is-courses', { ...initialValuesCourses.dropdown }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' }, '-=0.3')
+  .fromTo('.dropdown_menu-image.is-courses', { ...initialValuesCourses.image }, { scale: 1, duration: 0.3, ease: 'ease' }, '-=0.2')
+  .fromTo('.dropdown_menu-text-wrapper.is-courses', { ...initialValuesCourses.textWrapper }, { opacity: 1, y: '0%', duration: 0.3, ease: 'ease' }, '-=0.2');
 
 // Function to start the entrance animation and handle hover state
 function playEnterAnimationCourses() {
   // Restart the animation from the beginning
-  enterTimelineCourses.restart();
-  isAnimationReversedCourses = false;
-
-  // Set the id to the hovered state
-  navLinkCourses.classList.add('hovered');
+  setActiveDropdown('.dropdown_menu-component.is-courses', enterTimelineCourses, navLinkCourses);
 }
 
 // Function to reverse the exit animation and handle hover state
 function reverseExitAnimationCourses() {
-  // Hide only the target class .hero-navbar_dropdown instead of reversing the timeline
-  gsap.set('.hero-navbar_dropdown', { display: 'none' });
-
-  // Restore the initial values of the target classes
-  gsap.set('.dropdown_menu-component.is-courses', { ...initialValues.dropdown });
-  gsap.set('.dropdown_menu-image.is-courses', { ...initialValues.image });
-  gsap.set('.dropdown_menu-text-wrapper.is-courses', { ...initialValues.textWrapper });
-
-  isAnimationReversedCourses = true;
-
-  // Remove the hover state when the animation is reversed
-  navLinkCourses.classList.remove('hovered');
+  closeActiveDropdown('.dropdown_menu-component.is-courses', enterTimelineCourses, navLinkCourses);
 }
 
 // Add mouseenter event to trigger the entrance animation
@@ -220,15 +192,13 @@ enterTimelineCourses.eventCallback("onComplete", function() {
   isAnimationReversedCourses = true;
 });
 
-
-
 // DROPDOWN menu Academy
 var enterTimelineAcademy = gsap.timeline({ paused: true });
 var navLinkAcademy = document.getElementById('hero-navlink-academy');
 var isAnimationReversedAcademy = true; // Animation starts as reversed
 
 // Capture initial values for the reverseExitAnimationAcademy function
-var initialValues = {
+var initialValuesAcademy = {
   dropdown: { display: 'none', opacity: 0 },
   image: { scale: 1.3 },
   textWrapper: { opacity: 0, y: '7%' }
@@ -237,34 +207,19 @@ var initialValues = {
 // Add actions to the entrance animation with easing "ease"
 enterTimelineAcademy
   .fromTo('.hero-navbar_dropdown', { display: 'none', opacity: 0 }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' })
-  .fromTo('.dropdown_menu-component.is-academy', { ...initialValues.dropdown }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' }, '-=0.3')
-  .fromTo('.dropdown_menu-image.is-academy', { ...initialValues.image }, { scale: 1, duration: 0.3, ease: 'ease' }, '-=0.2')
-  .fromTo('.dropdown_menu-text-wrapper.is-academy', { ...initialValues.textWrapper }, { opacity: 1, y: '0%', duration: 0.3, ease: 'ease' }, '-=0.2');
+  .fromTo('.dropdown_menu-component.is-academy', { ...initialValuesAcademy.dropdown }, { display: 'block', opacity: 1, duration: 0.3, ease: 'ease' }, '-=0.3')
+  .fromTo('.dropdown_menu-image.is-academy', { ...initialValuesAcademy.image }, { scale: 1, duration: 0.3, ease: 'ease' }, '-=0.2')
+  .fromTo('.dropdown_menu-text-wrapper.is-academy', { ...initialValuesAcademy.textWrapper }, { opacity: 1, y: '0%', duration: 0.3, ease: 'ease' }, '-=0.2');
 
 // Function to start the entrance animation and handle hover state
 function playEnterAnimationAcademy() {
   // Restart the animation from the beginning
-  enterTimelineAcademy.restart();
-  isAnimationReversedAcademy = false;
-
-  // Set the id to the hovered state
-  navLinkAcademy.classList.add('hovered');
+  setActiveDropdown('.dropdown_menu-component.is-academy', enterTimelineAcademy, navLinkAcademy);
 }
 
 // Function to reverse the exit animation and handle hover state
 function reverseExitAnimationAcademy() {
-  // Hide only the target class .hero-navbar_dropdown instead of reversing the timeline
-  gsap.set('.hero-navbar_dropdown', { display: 'none' });
-
-  // Restore the initial values of the target classes
-  gsap.set('.dropdown_menu-component.is-academy', { ...initialValues.dropdown });
-  gsap.set('.dropdown_menu-image.is-academy', { ...initialValues.image });
-  gsap.set('.dropdown_menu-text-wrapper.is-academy', { ...initialValues.textWrapper });
-
-  isAnimationReversedAcademy = true;
-
-  // Remove the hover state when the animation is reversed
-  navLinkAcademy.classList.remove('hovered');
+  closeActiveDropdown('.dropdown_menu-component.is-academy', enterTimelineAcademy, navLinkAcademy);
 }
 
 // Add mouseenter event to trigger the entrance animation
